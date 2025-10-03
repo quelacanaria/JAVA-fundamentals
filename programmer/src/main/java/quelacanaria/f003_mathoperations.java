9fd82911ff1e5c6f1fc2edd33f6835b7af035194
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-03 13:59:13 +0800

    commit

---
+++ programmer/src/main/java/quelacanaria/f003_mathoperations.java
@@ -0,0 +1,32 @@
+package quelacanaria;
+
+public class f003_mathoperations {
+
+    //x=x+y; | x+=y;
+    //x=x-y; | x-=y;
+    //x=x*y; | x*=y;
+    //x=x/y; | x/=y;
+    //x=x%y; | x%=y;
+    //x++; increment
+    //x--; decrement
+
+    //P-E-M-D-A-S
+//    P - arenthesis
+//    E - xponent
+//    M - ultiplication
+//    D - ivision
+//    A - ddition
+//    S - ubtraction
+    public static void main(String[] args) {
+
+    double result = 3 + 4 * (7 - 5) / 2.0;
+//    double result = 3 + 4 * (7 - 5) / 2.0;
+//    double result = 3 + 4 * 2 / 2.0;
+//    double result = 3 + 8 / 2.0;
+//    double result = 3 + 4.0;
+//    double result = 7.0;
+    System.out.println(result); //7.0
+
+    }
+
+}
