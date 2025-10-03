5da15f396c1edd0a03d55061d954906dcc9b405e
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-03 12:06:57 +0800

    commit

---
+++ programmer/src/main/java/quelacanaria/f001_Primitive.java
@@ -0,0 +1,41 @@
+package quelacanaria;
+
+public class f001_Primitive {
+
+    public static void main(String[] args) {
+        // Primitive = simple value stored directly in memory(stack)
+        // Reference = memory address (stack) that points to the (heap)
+
+        // Primitive-int, double, char, boolean, short, long, float
+        // Reference-String, array, object
+
+        // creating a variable
+
+        // 1. datatype - int, double, long, char, boolean, short, String,  array, object, float
+        // 2. variable declaration - anything in abc
+        // 3. assignment - anything as long as it fits the datatype
+
+        int grade = 90;
+        int topNumber = 1;
+        int numberStudents = 50;
+        double price = 500_000.00;
+        char remarks = 'A';
+        boolean pass = true;
+        String name = "Sarah Discaya";
+        String luxuryCar = "Bently Bentayga";
+        String passed = "";
+        if(pass == true){
+            passed = "Pass";
+        }else{
+            passed = "passed away";
+        }
+
+        System.out.println("\n"+name + " was once a top " + topNumber + " n her class of over "
+                + numberStudents + " . Out of those, only 30 managed to "+ passed + " and she earned an impressive " + remarks + "+ "
+                + grade + " \nAs a reward for her achievement, her father gave her $" + price
+                + " to buy her dream car a " + luxuryCar);
+
+
+    }
+
+}
