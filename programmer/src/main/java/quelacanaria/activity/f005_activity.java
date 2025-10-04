298b5c467047b85ea813a64a4cd9c3f7ddbde6da
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-04 13:12:17 +0800

    commit

--- programmer/src/main/java/quelacanaria/activity/f005_activity.java
+++ programmer/src/main/java/quelacanaria/activity/f005_activity.java
@@ -1,9 +1,29 @@
 package quelacanaria.activity;
 
+import java.util.Scanner;
+
 public class f005_activity {
 
     //circumference = 2 * Math.PI * raduis;
     //area = Math.PI * Math.pow(raduis,2);
-    //volume = (4.0/3.0)
+    //volume = (4.0/3.0) * Math.PI * Math.pow(raduis,3);
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double circumference;
+        double raduis;
+        double area;
+        double volume;
+
+        raduis = scan.nextDouble();
+
+        circumference = 2 * Math.PI * raduis;
+        area = Math.PI * Math.pow(raduis,2);
+        volume = (4.0/3.0) * Math.PI * Math.pow(raduis,3);
+
+    }
 
 }
+
