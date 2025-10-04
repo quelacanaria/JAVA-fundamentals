75ac8bae2d6888ccb4ae07f607077625da99b524
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-04 13:23:45 +0800

    commit

--- programmer/src/main/java/quelacanaria/activity/f005_activity.java
+++ programmer/src/main/java/quelacanaria/activity/f005_activity.java
@@ -17,11 +17,18 @@ public class f005_activity {
         double area;
         double volume;
 
+        System.out.print("Enter the raduis of the circle: ");
         raduis = scan.nextDouble();
 
         circumference = 2 * Math.PI * raduis;
         area = Math.PI * Math.pow(raduis,2);
         volume = (4.0/3.0) * Math.PI * Math.pow(raduis,3);
+
+        System.out.println("the circumference of the circle is: " + circumference);
+        System.out.println("the area of the circle is: " + area);
+        System.out.println("the volume of the circle is             " + volume);
+
+        scan.close();
 
     }
 
