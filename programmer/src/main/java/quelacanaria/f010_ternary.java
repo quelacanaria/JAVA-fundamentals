552971181d8392c91ffefd616aa748e269711e3f
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-06 00:15:23 +0800

    Create f010_ternary.java

---
+++ programmer/src/main/java/quelacanaria/f010_ternary.java
@@ -0,0 +1,14 @@
+package quelacanaria;
+
+public class f010_ternary {
+    //ternary operator ?= Return 1 0f 2 values if a condition is true
+    //variable = (condition) ? ifTrue : ifFalse
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+
