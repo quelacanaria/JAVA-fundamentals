9e9446298c1b81393e5fbd598fe572a8af75f6f1
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-06 14:14:44 +0800

    commit

--- programmer/src/main/java/quelacanaria/activity/f015_activity.java
+++ programmer/src/main/java/quelacanaria/activity/f015_activity.java
@@ -11,12 +11,15 @@ public class f015_activity {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter your age: ");
+        age = scan.nextInt();
 
-        while(age < -1){
+        while(age < 0){
+            System.out.println("Invalid you haven't born yet");
             System.out.print("Enter your age: ");
             age = scan.nextInt();
         }
 
+        System.out.println("You are " + age + " years old");
 
     }
 
