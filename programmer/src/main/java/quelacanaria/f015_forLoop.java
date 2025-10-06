e31a84b7bc86c05c5535a436abe7945c43af2d6e
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-06 22:53:54 +0800

    commit

--- programmer/src/main/java/quelacanaria/f015_forLoop.java
+++ programmer/src/main/java/quelacanaria/f015_forLoop.java
@@ -1,14 +1,40 @@
 package quelacanaria;
 
+import java.util.Scanner;
+
 public class f015_forLoop {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Scanner scan = new Scanner(System.in);
+        int max;
+        System.out.println("Increment by 1");
         for(int i = 0; i < 10; i++){
 
             System.out.println(i);
 
         }
+        System.out.println("Increment by 2");
+        for(int i = 0; i < 10; i+=2){
+
+            System.out.println(i);
+
+        }
+        System.out.print("How many numbers do you want to print?: ");
+        max = scan.nextInt();
+        for(int i = 0; i < max; i++){
+
+            System.out.println(i);
+
+        }
+        System.out.println("End of the program\n");
+        for(int i = 10; i > 0; i--){
+
+            System.out.println(i);
+            Thread.sleep(1000);
+
+        }
+        System.out.println("Happy new year");
 
     }
 
