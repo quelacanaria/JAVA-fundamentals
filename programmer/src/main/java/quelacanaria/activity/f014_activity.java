efff205257cf035b9bfff1ff62a7839b6d5a1d1c
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-06 15:16:13 +0800

    commit

--- programmer/src/main/java/quelacanaria/activity/f014_activity.java
+++ programmer/src/main/java/quelacanaria/activity/f014_activity.java
@@ -7,18 +7,30 @@ public class f014_activity {
     public static void main(String[] args) {
 
         String response = "";
-
+        int age = 0;
         Scanner scan = new Scanner(System.in);
 
         while(!response.equals("Q")){
 
             System.out.println("You are playing a game");
-            System.out.println("press Q to quit");
+            System.out.print("press Q to quit: ");
             response = scan.next().toUpperCase();
 
         }
 
         System.out.println("You have quit the game");
+
+        System.out.println("\nenter +numbers only");
+        System.out.print("Enter your age: ");
+        age = scan.nextInt();
+
+        while(age < 0){
+            System.out.println("\nnvalid you haven't born yet");
+            System.out.print("Enter your age: ");
+            age = scan.nextInt();
+        }
+
+        System.out.println("You are " + age + " years old");
 
         scan.close();
 
