c59f1c35236b01b0622e889cf38c2c81d4a75799
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-07 00:48:54 +0800

    commit

---
+++ programmer/src/main/java/quelacanaria/f019_overloadedMethods.java
@@ -0,0 +1,20 @@
+package quelacanaria;
+
+public class f019_overloadedMethods {
+    public static void main(String[] args) {
+        //methods that share the same name but different parameters
+
+        System.out.println(add(2,2,10));
+        System.out.println(bakePizza("flat bread "));
+
+    }
+    static double add(int a, int b){
+        return a + b;
+    }
+    static double add(int a, int b, int c){
+        return a + b + c;
+    }
+    static String bakePizza(String bread){
+        return bread + "pizza";
+    }
+}
