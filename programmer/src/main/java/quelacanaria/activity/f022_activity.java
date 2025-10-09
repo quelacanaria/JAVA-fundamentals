64ff78992db6a32ca778895fc98bb33ee686ba62
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-09 17:17:31 +0800

    commit

--- programmer/src/main/java/quelacanaria/activity/f022_activity.java
+++ programmer/src/main/java/quelacanaria/activity/f022_activity.java
@@ -13,16 +13,48 @@ public class f022_activity {
         String aiPlayer;
         String player;
         int attempts = 1;
+        String playAgain = "yes";
         String[] rps = {"rock", "paper", "scissors"};
 
         aiPlayer = rps[random.nextInt(0,3)];
+        do {
 
-        System.out.print("Enter your move (rock, paper, scissors) ");
-        player = scan.nextLine().toLowerCase();
+            System.out.println("Rock, Paper, Scissors Game");
 
-        if(!player.equals("rock")&&!player.equals("paper")&&!player.equals("scissors")){
-            System.out.println("Invalid choice");
-        }
+            System.out.print("Player pick: ");
+            player = scan.nextLine().toLowerCase();
 
+            if (!player.equals("rock") && !player.equals("paper") && !player.equals("scissors")) {
+
+                System.out.println("Invalid pick");
+            }
+
+            System.out.println("Computer pick: " + aiPlayer);
+            ;
+
+            if (player.equals(aiPlayer)) {
+
+                System.out.println("It's a tie!!");
+
+            } else if ((player.equals("scissors") && aiPlayer.equals("paper")) ||
+                    (player.equals("paper") && aiPlayer.equals("rock")) ||
+                    (player.equals("rock") && aiPlayer.equals("scissors"))) {
+
+                System.out.println("You win!!");
+
+            } else {
+
+                System.out.println("You lose😒😒");
+
+            }
+
+            System.out.println("Play again?? (yes or no): ");
+            playAgain = scan.nextLine().toLowerCase();
+
+        }while(playAgain.equals("yes"));
+
+        System.out.println("Thanks for playing!!");
+
+        scan.close();
     }
 }
