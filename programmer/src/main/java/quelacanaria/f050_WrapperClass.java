f75ab91f95a35343af2fe506ec069410f00ee6ec
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-10-11 22:36:22 +0800

    commit

--- programmer/src/main/java/quelacanaria/f050_WrapperClass.java
+++ programmer/src/main/java/quelacanaria/f050_WrapperClass.java
@@ -4,6 +4,7 @@ package quelacanaria;
 //Allows use of Collections Framework and static Utility Methods.
 public class f050_WrapperClass {
     public static void main(String[] args) {
+
     //Autoboxing
         Integer i = 1;
         Double d = 2.00;
@@ -16,12 +17,25 @@ public class f050_WrapperClass {
 //        char C = c;
 //        boolean B = b;
 
-        String w = i.toString();
-        String x = d.toString();
-        String y = c.toString();
-        String z = b.toString();
+//        String w = i.toString();
+//        String x = d.toString();
+//        String y = c.toString();
+//        String z = b.toString();
 
-        System.out.println();
+//        int w = Integer.parseInt("123");
+//        double x = Double.parseDouble("3.14");
+//        boolean y = Boolean.parseBoolean("true");
+        char z = "1".charAt(0);
+
+
+//        String q = Integer.toString(123);
+//        String w = Double.toString(20);
+//        String e = Boolean.toString(true);
+//        String r = Character.toString('c');
+
+        System.out.println(Character.isLetter(z));
+        System.out.println(Character.isUpperCase(z));
+
 
     }
 
